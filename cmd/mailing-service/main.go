9f4a8bbc7d9563bda92599a3861225142bcb6a7e
MUSQQQ <super.walczak@gmail.com>
2024-05-19 18:36:58 +0200

    add logs

--- cmd/mailing-service/main.go
+++ cmd/mailing-service/main.go
@@ -9,22 +9,30 @@ import (
 )
 
 func main() {
-	logrus.Info("starting mailing-service")
+	log := logrus.NewEntry(logrus.New())
+	log.Info("starting mailing-service")
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
 
+	log.Info("parsing config")
 	cfg := config.Parse()
 
+	log.Info("initializing database")
 	db := service.NewDB(cfg.DBConfig)
 
+	log.Info("initializing service")
 	srv := service.NewService(db)
 
+	log.Info("initializing router")
 	router := service.NewRouter(cfg.RouterConfig)
 
+	log.Info("running router")
 	router.RegisterHandlers(srv)
 	go router.Run(ctx)
 
+	log.Info("initializing cron")
 	cron := service.NewCron(cfg.CronConfig, db)
+	log.Info("running cron")
 	go cron.Run(ctx)
 	<-ctx.Done()
 }
--- service/handlers.go
+++ service/handlers.go
@@ -10,69 +10,76 @@ import (
 )
 
 func (s *Service) CreateMailingDetails(w http.ResponseWriter, r *http.Request) {
-	logrus.Info("start handling CreateMailingDetails")
+	log := logrus.NewEntry(logrus.New())
+	log.Info("handling CreateMailingDetails")
 	req := &CreateMailingDetailsRequest{}
 
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		logrus.WithError(err).Error("failed to decode request body")
+		log.WithError(err).Error("failed to decode request body")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	log = log.WithField("request_body", req)
 
 	mailingDetails, err := req.toMailingDetails()
 	if err != nil {
-		logrus.WithError(err).Error("failed to map request to mailing details")
+		log.WithError(err).Error("failed to map request to mailing details")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	logrus.WithField("details", mailingDetails).Info("mapped")
+	log.WithField("mapped_details", mailingDetails)
 
 	err = s.db.CreateMailingDetails(*mailingDetails)
 	if err != nil {
-		logrus.WithError(err).Error("failed to save mailing details")
+		log.WithError(err).Error("failed to save mailing details")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	log.Info("saved mailing details")
 	w.WriteHeader(http.StatusOK)
 }
 
 func (s *Service) SendMailing(w http.ResponseWriter, r *http.Request) {
-	logrus.Info("start handling SendMailingRequest")
+	log := logrus.NewEntry(logrus.New())
+	log.Info("handling SendMailing")
 	req := &SendMailingRequest{}
 
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		logrus.WithError(err).Error("failed to decode request body")
+		log.WithError(err).Error("failed to decode request body")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	log = log.WithField("request_body", req)
 
 	details, err := s.db.GetMailingDetailsByMailingID(req.MailingID)
 	if err != nil {
-		logrus.WithError(err).Error("failed to get mailing details")
+		log.WithError(err).Error("failed to get mailing details")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	logrus.WithField("details", len(details)).Info("got details to send")
+	log = log.WithField("mailing_details_found", len(details))
 
 	IDsSent, err := s.SendEmails(details)
 	if err != nil {
-		logrus.WithError(err).Error("failed to send emails")
+		log.WithError(err).Error("failed to send emails")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	log = log.WithField("emails_sent", len(IDsSent))
 
 	filters := &Filters{
 		clauses: []string{},
 	}
 	err = s.db.DeleteMailingDetails(filters.ByIDs(IDsSent).clauses)
 	if err != nil {
-		logrus.WithError(err).Error("failed to delete mailing details")
+		log.WithError(err).Error("failed to delete mailing details")
 	}
 
 	if len(details) > len(IDsSent) {
+		log.Warn("failed to send some emails")
 		info, _ := json.Marshal(map[string]string{
 			"info": "partial_success",
 		})
@@ -82,28 +89,37 @@ func (s *Service) SendMailing(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	log.Info("all emails successfully sent")
 	w.WriteHeader(http.StatusOK)
 }
 
 func (s *Service) DeleteMailingDetails(w http.ResponseWriter, r *http.Request) {
+	log := logrus.NewEntry(logrus.New())
+	log.Info("handling DeleteMailingDetails")
+
 	mailingID := chi.URLParam(r, "mailingID")
 	if mailingID == "" {
-		logrus.Warn("mailingID not found")
+		log.Warn("mailingID not found")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	log = log.WithField("mailing_id", mailingID)
+
 	id, err := strconv.Atoi(mailingID)
 	if err != nil {
-		logrus.Warn("incorrect mailingID")
+		log.Warn("incorrect mailingID")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
 	filters := Filters{clauses: []string{}}
 	err = s.db.DeleteMailingDetails(filters.ByMailingID(id).clauses)
 	if err != nil {
-		logrus.Warn("failed to delete mailing details")
+		log.Warn("failed to delete mailing details")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	log.Info("mailing details deleted")
 	w.WriteHeader(http.StatusOK)
 }
